f65239cea3566d0cb7f5495f2050e12d976978d8
Kevin Sajan <kevsaj@gmail.com>
2021-03-04 00:13:47 -0500

    create service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    '/',
+    'index.html',
+    'styles.css',
+    'index.js',
+    'manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+  ];
